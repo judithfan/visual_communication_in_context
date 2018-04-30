a3a8bb37c0414a8551f96158c0419b5fbc429d37
Judy Fan <judithfan@gmail.com>
2018-04-29 20:17:03 -0700

    bundle in support for human annotations

--- models/RSA.py
+++ models/RSA.py
@@ -3,34 +3,6 @@ import os
 import thread
 import numpy as np
 
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics combined --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics S1 --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics S0 --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics combined --production nocost
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics S1 --production nocost
-# webppl BDA.wppl --require ./refModule/ -- --perception human_full25k --pragmatics S0 --production nocost
-#
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics combined --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics S1 --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics S0 --production cost
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics combined --production nocost
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics S1 --production nocost
-# webppl BDA.wppl --require ./refModule/ -- --perception sketch_unroll_full25k --pragmatics S0 --production nocost
-
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_combined_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_S1_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_S0_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_combined_nocost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_S1_nocost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting human_full25k_S0_nocost
-
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_combined_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_S1_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_S0_cost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_combined_nocost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_S1_nocost
-# webppl evaluate.wppl --require ./refModule/ -- --paramSetting sketch_unroll_full25k_S0_nocost
-
 def run_bda(perception, pragmatics, production):
     cmd_string = 'webppl BDA.wppl --require ./refModule/ -- --perception {} --pragmatics {} --production {}'.format(perception, pragmatics, production)
     print 'Running: {}'.format(cmd_string)
@@ -47,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument('--wppl', type=str, help='options: BDA | evaluate', default='BDA')
     parser.add_argument('--perception', type=list, \
                         help='which generation of sketch photo adaptor? options: sketch_unroll_full25k | human_full25k', \
-                        default=['sketch_unroll_full25k','human_full25k'])
+                        default=['human_full25k'])
     parser.add_argument('--pragmatics', type=list, \
                         help='which pragmatics model? options: combined | S1 | S0 ', \
                         default=['combined','S1','S0'])
