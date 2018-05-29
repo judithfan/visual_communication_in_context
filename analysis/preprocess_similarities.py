c538f959482336bc67463ef300b3c62039e67ff6
Judy Fan <judithfan@gmail.com>
2018-05-29 15:05:02 -0700

    add preprocessing script to handle multiple splits

---
+++ analysis/preprocess_similarities.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import argparse
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+def clean_dirlist(x):
+    return [i for i in x if i != '.DS_Store']
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_to_src_similarities', type=str, help='path to dir containing similarities, \
+                        multiple crossval splits expected', default='../pix2svg-similarities-052918')
+    parser.add_argument('--split_type', type=str, help='type of split and learning objective during adaptor training', default='balancedavg')
+
+    args = parser.parse_args()
+
+    ## destination directories
+    path_to_test_examples = './'
+    path_to_dest_similarities = '../models/refModule/json/'
+
+    ## get list of layers
+    contents = clean_dirlist(os.listdir(args.path_to_src_similarities))
+    layers = ['pool1','conv42','fc6']
+    split_dir = 'train_test_split'
+
+    ## copy similarities over to the models/refModule/json location, appending the split number to the split_type tag, e.g., "balancedavg1"
+    for i,layer in enumerate(layers):
+        splits = clean_dirlist(os.listdir(os.path.join(args.path_to_src_similarities,layer)))
+        for split_num in splits:
+            fname ='similarity-{}{}-multimodal_{}-raw.json'.format(args.split_type,split_num,layer)
+            out_path = os.path.join(path_to_dest_similarities,fname)
+            in_path = os.path.join(args.path_to_src_similarities,layer,split_num,'dump.json')
+            print 'Copying {} to {}'.format(in_path, out_path)
+            shutil.copy(in_path,out_path)
+
+    ## copy test_examples to this analysis dir
+    template = 'pilot2_multimodal_test_examples.json'
+
+    splits = clean_dirlist(os.listdir(os.path.join(args.path_to_src_similarities,split_dir)))
+    for split_num in splits:
+        fname = 'pilot2_multimodal_{}{}_test_examples.json'.format(args.split_type,split_num)
+        out_path = os.path.join('./',fname)
+        in_path = os.path.join(args.path_to_src_similarities,split_dir,split_num,'test_split.json')
+        print 'Copying {} to {}'.format(in_path, out_path)
+        shutil.copy(in_path,out_path)
