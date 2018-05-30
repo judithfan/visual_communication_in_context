a8178bab54b368b3776e3d7556e645b74b296aab
Judy Fan <judithfan@gmail.com>
2018-05-30 08:49:00 -0700

    add nesting

--- analysis/prep4RSA.py
+++ analysis/prep4RSA.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
            bda_out_path = '../models/bdaOutput/multimodal_{}_{}{}'.format(layer,split_type,split_num)
            if not os.path.exists(bda_out_path):
                os.makedirs(bda_out_path)
+               os.makedirs(os.path.join(bda_out_path,'raw'))
 
     ## also for humans
     for split_num in splits:
@@ -29,3 +30,4 @@ if __name__ == "__main__":
         bda_out_path = '../models/bdaOutput/human_{}{}'.format(split_type,split_num)
         if not os.path.exists(bda_out_path):
             os.makedirs(bda_out_path)
+            os.makedirs(os.path.join(bda_out_path,'raw'))
