aafbee6106064df433468e56201f5f844dcd6763
Judy Fan <judithfan@gmail.com>
2018-05-30 23:29:57 -0700

    prep4RSA for layer test

--- analysis/prep4RSA.py
+++ analysis/prep4RSA.py
@@ -3,9 +3,11 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    layers = ['pool1','conv42','fc6']
+    # layers = ['pool1','conv42','fc6']
+    layers = ['fc6']
     split_type = 'balancedavg'
-    splits = map(str,np.arange(1,6))
+    splits = ['16'.'32','64','128','256','512']
+    # splits = map(str,np.arange(1,6))
 
     ## for the adapted encoder
     for layer in layers:
@@ -13,7 +15,7 @@ if __name__ == "__main__":
             cmd_string = "python generate_bdainput.py --adaptor_type multimodal_{} --split_type {}{}".format(layer,split_type,split_num)
             print '{}'.format(cmd_string)
             os.system(cmd_string)
-            
+
             ## make bdaOutput subdirectories to save out to
             bda_out_path = '../models/bdaOutput/multimodal_{}_{}{}'.format(layer,split_type,split_num)
             if not os.path.exists(bda_out_path):
