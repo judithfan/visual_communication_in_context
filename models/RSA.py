112fdd89ed6161a8a70080a0f6a85218f3f46cf1
Judy Fan <judithfan@gmail.com>
2018-07-09 17:05:37 -0700

    adapt wrapper around BDA-enumerate to handle batches

--- models/RSA.py
+++ models/RSA.py
@@ -16,8 +16,8 @@ def run_bda(perception, pragmatics, production, split_type):
     print 'Running: {}'.format(cmd_string)
     thread.start_new_thread(os.system,(cmd_string,))
 
-def run_bda_enumerate(perception, pragmatics, production):
-    cmd_string = 'webppl BDA-enumerate.wppl --require ./refModule/ -- --perception {} --pragmatics {} --production {}'.format(perception, pragmatics, production)
+def run_bda_enumerate(simScaling, splitType):
+    cmd_string = 'webppl BDA-enumerate.wppl --require ./refModule/ -- --simScaling {} --splitType {}'.format(simScaling, splitType)
     print 'Running: {}'.format(cmd_string)
     thread.start_new_thread(os.system,(cmd_string,))
 
@@ -42,6 +42,14 @@ if __name__ == "__main__":
     parser.add_argument('--split_type', nargs='+', type=str, \
                         help='option: splitbyobject | alldata | balancedavg',\
                         default = 'balancedavg')
+    parser.add_argument('--sim_scaling', type=float, \
+                        help='for BDA-enumerate only: this is the upper bound for the simScaling param. \
+                              We will sweep through values from (1, sim_scaling) in step size of 2',\
+                        default = 200.0) 
+    parser.add_argument('--step_size', type=float, \
+                        help='for BDA-enumerate only: this is the step size we will use to march through \
+                              the simScaling range',\
+                        default = 2.0)   
 
     args = parser.parse_args()
 
@@ -49,18 +57,18 @@ if __name__ == "__main__":
     production = args.production
     pragmatics = args.pragmatics
     split_type = args.split_type
+    sim_scaling = args.sim_scaling
+    step_size = args.step_size
 
     assert args.wppl in ['BDA','evaluate', 'BDA-enumerate']
 
     ## first run BDA-enumerate.wppl
     if 'BDA-enumerate' in args.wppl:
-        for perc in perception:
-            for prag in pragmatics:
-                for prod in production:
-                    for split in split_type:
-                        run_bda_enumerate(perc,prag,prod)
+        ss_range = np.arange(1,sim_scaling,step_size)
+        for i,ss in enumerate(ss_range):        
+            for split in split_type:
+                run_bda_enumerate(ss,split)
 
-    
     ## first run BDA.wppl
     elif 'BDA' in args.wppl:
         for perc in perception:
@@ -68,7 +76,6 @@ if __name__ == "__main__":
                 for prod in production:
                     for split in split_type:
                         run_bda(perc,prag,prod,split)
-
 
     elif 'evaluate' in args.wppl:
         ## then on output, run evaluate.wppl
