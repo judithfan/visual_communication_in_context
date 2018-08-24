a0161812c13619365271d95c7f8d492c0c1445ee
Judy Fan <judithfan@gmail.com>
2018-08-24 15:09:37 -0700

    bugfix

--- models/RSA_job_patcher.py
+++ models/RSA_job_patcher.py
@@ -3,22 +3,25 @@ import numpy as np
 
 if __name__ == "__main__":
 
-	## which have already been run
-	enumerate_dir = os.path.join(os.getcwd(),'enumerateOutput')
-	results_dir = os.path.join(enumerate_dir,os.listdir(results_dir)[0])
-	print 'Results dir is: {}'.format(results_dir)
+## which have already been run
+enumerate_dir = os.path.join(os.getcwd(),'enumerateOutput')
+results_dir = os.path.join(enumerate_dir,os.listdir(enumerate_dir)[0])
+print 'Results dir is: {}'.format(results_dir)
 
-	completed_inds = map(int,[i.split('_')[2] for i in os.listdir(results_dir)])
+completed_inds = map(int,[i.split('_')[2] for i in os.listdir(results_dir)])
 
-	## which still need to be run
-	still_to_run = [i for i in np.arange(1,200,2) if i not in completed_inds]
+## which still need to be run
+still_to_run = [i for i in np.arange(1,200,2) if i not in completed_inds]
+print 'These still yet to run: {}'.format(still_to_run)
 
-	## identify how many different new RSA.py calls to make
-	## by getting list of straggler lower bounds
-	straggler_lb = np.where(np.diff(still_to_run) !=2)[0]
+## identify how many different new RSA.py calls to make
+## by getting list of straggler lower bounds
+straggler_lb = np.where(np.diff(still_to_run) !=2)[0]
+if len(straggler_lb)==0:
+	print 'No stragglers, run them all.'
 
-	## which split?
-	which_split = os.listdir(results_dir)[0].split('_')[0]
+## which split?
+which_split = os.listdir(results_dir)[0].split('_')[0]
 
 	## go through and run RSA.py for earliest to latest in list of still to run
 	lb = still_to_run[0]
