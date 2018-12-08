b51ce308bc9486338b8318e506d413406a8cde9c
Judy Fan <judithfan@gmail.com>
2018-12-07 21:30:39 -0800

    discussion edits

--- models/enumerateOutput/compile.sh
+++ models/enumerateOutput/compile.sh
@@ -4,5 +4,5 @@ do
   cd raw/balancedavg${i}
   head -1 balancedavg${i}_sim_1_enumerate.csv > ../../balancedavg${i}.csv;
   tail -n +2 -q balancedavg${i}_sim_* >> ../../balancedavg${i}.csv;
-  cd ../../
+  cd ../..
 done
